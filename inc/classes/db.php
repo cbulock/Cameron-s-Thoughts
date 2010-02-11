2fe19a22e16a66a4193e99a7f160c5441050ad99
Cameron Bulock <cameron@bulock.com>
2010-02-11 16:32:35 -0500

    updates to db class

--- inc/classes/db.php
+++ inc/classes/db.php
@@ -3,6 +3,7 @@
 class DB {
 
 private $queryCount;
+private $lastQuery;
 private $link;
 
 /**********************************
@@ -12,39 +13,38 @@ private $link;
 public function getTable($table, $options = array()) {
  $defaults = array(
   'where' => '1',
-  'orderBy' => '`id` DESC'
+  'orderBy' => '`id` DESC',
+  'limit' => '5000',
+  'key' => 'id'
  );
- $options = setOptions($options,$defaults);
- selectDatabase($table, $options['database']);
- $sql = 'SELECT * FROM `'.$table.'` WHERE '.sqlClean($options['where']).' ORDER BY '.$options['orderBy'];
- return sqlProcessMulti($sql);
+ $options = $this->setOptions($options,$defaults);
+ $sql = 'SELECT * FROM `'.$table.'` WHERE '.$options['where'].' ORDER BY '.$options['orderBy'].' LIMIT '.$options['limit'];
+ return $this->sqlProcessMulti($sql,$options['key']);
 }
 
 public function getItem($table, $value, $options = array()) {
  $defaults = array(
-  $field => 'id'
+  'field' => 'id'
  );
- $options = setOptions($options,$defaults);
- selectDatabase($table, $options['database']);
- $sql = 'SELECT * FROM `'.$table.'` WHERE `'.sqlClean($options['field']).'` = \''.sqlClean($value).'\'';
- return sqlProcess($sql);
+ $options = $this->setOptions($options,$defaults);
+ $sql = 'SELECT * FROM `'.$table.'` WHERE `'.$this->sqlClean($options['field']).'` = \''.$this->sqlClean($value).'\'';
+ return $this->sqlProcess($sql);
 }
 
 public function insertItem($table, $data, $options = array()) {
  $defaults = array();
- $options = setOptions($options,$defaults);
- selectDatabase($table, $options['database']);
+ $options = $this->setOptions($options,$defaults);
  foreach($data as $key => $val)
  {
   $keys .= '`'.$key.'`,';
-  $vals .= '\''.sqlclean($val).'\', ';
+  $vals .= '\''.$this->sqlclean($val).'\', ';
  }
  $keys = rtrim($keys, ', ');
  $vals = rtrim($vals, ', ');
  $sql = 'INSERT INTO `'.$table.'` (';
  $sql .= $keys.') VALUES ('.$vals.')';
- if (sqlQuery($sql)) {
-  return mysql_insert_id();
+ if ($this->sqlQuery($sql)) {
+  return mysql_insert_id($this->link);
  } 
  else {
   return FALSE;
@@ -53,53 +53,36 @@ public function insertItem($table, $data, $options = array()) {
 
 public function updateItem($table, $value, $data, $options = array()) {
  $defaults = array(
-  $field => 'id'
+  'field' => 'id'
  );
- $options = setOptions($options,$defaults);
- selectDatabase($table, $options['database']);
+ $options = $this->setOptions($options,$defaults);
+ $sql = 'UPDATE `'.$table.'` SET ';
  foreach($data as $key => $val) {
-  $sql .= '`'.$key.'`=\''.sqlclean($val).'\', ';
+  $sql .= '`'.$key.'`=\''.$this->sqlclean($val).'\', ';
  }
  $sql = rtrim($sql, ', ');
- $sql .= ' WHERE `'.$field.'`=\''.$value.'\'';
- return sqlQuery($sql); //investigate if more should be returned
+ $sql .= ' WHERE `'.$options['field'].'`=\''.$value.'\'';
+ return $this->sqlQuery($sql); //investigate if more should be returned
 }
 
 public function deleteItem($table, $value,  $options = array()) { 
  $defaults = array(
-  $field => 'id'
+  'field' => 'id'
  );
  $options = setOptions($options,$defaults);
- selectDatabase($table, $options['database']);
  $sql = 'DELETE FROM `'.$table.'` WHERE `'.sqlClean($options['field']).'` = \''.sqlclean($value) . '\'';
  return sqlQuery($sql); //investigate if more should be returned
 }
 
 private function sqlQuery($sql) {
- $queryCount++;
+ $this->queryCount++;
+ $this->lastQuery = $sql;
  return mysql_query($sql);
 }
 
 /**********************************
    Helper Functions
 **********************************/
-
-private function selectDatabase($table, $db=NULL) {
- mysql_select_db(determineDatabase($table, $db),$this->link);
-}
-
-private function determineDatabase($table, $db=NULL) { //$table can be overloaded with $db value
- if ($db) return $db;
- $databases['cbulock_accesslog'] = array('cbulock_accesslog','referers','sessions');
- $databases['cbulock_cbulock'] = array('cbulock_cbulock','ads','ads_cat','blockedips','guid_admins','images','quotes','styles','users','comments');
- $databases['cbulock_mt2'] = array('cbulock_mt2','mt_blog','mt_entry');
- $databases['cbulock_ct3'] = array('cbulock_ct3','settings');
- foreach ($databases as $dbname => $database) {
-  foreach ($database as $tablename) {
-   if ($tablename == $table) return $dbname;
-  }
- }
-}
 
 private function setOptions($options, $defaults) {
  foreach($defaults as $option => $value)
@@ -110,22 +93,22 @@ private function setOptions($options, $defaults) {
 }
 
 private function sqlProcess($sql) {//It may be more consistant to just use sqlProcessMulti for everything
- $sqlReturn = sqlQuery($sql);
+ $sqlReturn = $this->sqlQuery($sql);
  if (!$sqlReturn) return false;
  $row = mysql_fetch_array($sqlReturn);
  foreach($row as $key => $value) {
-  $result[$key] = htmlParse(stripslashes($value));
+  $result[$key] = $this->htmlParse(stripslashes($value));
  }
  return $result;
 }
 
-private function sqlProcessMulti($sql) {
- $sqlReturn = sqlQuery($sql);
+private function sqlProcessMulti($sql,$sortkey = 'id') {
+ $sqlReturn = $this->sqlQuery($sql);
  if (!$sqlReturn) return false;
  while ($row = mysql_fetch_array($sqlReturn)) {
-  $id = $row['id'];//I don't like this so much, depends on tables having an id column
+  $id = $row[$sortkey];
   foreach($row as $key => $value) {
-   $result[$id][$key] = htmlParse(stripslashes($value));
+   $result[$id][$key] = $this->htmlParse(stripslashes($value));
   }
  }
  return $result;
@@ -151,8 +134,9 @@ private function sqlClean($str) {
    Core Functions
 **********************************/
 
-public function __construct($host,$user,$pass) {
+public function __construct($host,$user,$pass,$db) {
  $this->link = mysql_connect($host,$user,$pass);
+ mysql_select_db($db,$this->link);
 }
 
 
@@ -160,21 +144,12 @@ public function __construct($host,$user,$pass) {
    Class Management
 **********************************/
 public function getQueryCount() {
- return $queryCount;
+ return $this->queryCount;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
+public function getLastQuery() {
+ return $this->lastQuery;
+}
 
 //End DB
 }
