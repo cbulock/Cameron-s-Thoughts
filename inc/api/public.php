bc0963ee2fbdd0c2a487875986c254037e646ff1
Cameron Bulock <cameron@bulock.com>
2010-02-24 22:44:03 -0500

    finally got the api json wrapper to cleaning call the api methods.  This is complete and functioning now

--- inc/api/public.php
+++ inc/api/public.php
@@ -8,26 +8,10 @@ if (strpos($request,'/')) {
 $method = explode('.',$request);
 $method = explode('/',$method[0]);
 $method = $method[0];
-//print_r($url_parameters[0]);
-switch(count($url_parameters)) {//I truely hate this and need to find a better way
- case 1:
-  $result = $ct->$method($url_parameters[0],$_REQUEST);
-//$result = $ct->$method($url_parameters[0]);
-// echo $ct->getLastQuery();
- break;
- case 2:
-  $result = $ct->$method($url_parameters[0],$url_parameters[1],$_REQUEST);
- break;
- case 3:
-  $result = $ct->$method($url_parameters[0],$url_parameters[1],$url_parameters[2],$_REQUEST);
- break;
- case 4:
-  $result = $ct->$method($url_parameters[0],$url_parameters[1],$url_parameters[2],$url_parameters[3],$_REQUEST);
- break;
- default:
-  $result = $ct->$method($_REQUEST);
- break;
-}
+
+$url_parameters[] = $_REQUEST;
+
+$result = call_user_func_array(array($ct,$method),$url_parameters);
 
 echo json_encode($result);
 ?>
