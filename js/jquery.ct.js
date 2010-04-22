92a839878ff2812f54b9ad630be72f778ba2ccc5
Cameron Bulock <cameron@bulock.com>
2010-04-22 19:52:46 -0400

    added all current api methods to the js lib

--- js/jquery.ct.js
+++ js/jquery.ct.js
@@ -1,35 +1,70 @@
 (function($){
  $.ct = {
   apiClient : function (method, req, opt) {
+   type = 'GET';
    if (opt) {
     type = 'POST';
-   }
-   else {
-    type = 'GET';
    }
    url = '/api/' + method;
    for (i=0;i<=req.length-1;i++) {
     url = url+'/'+req[i];
    }
+   url = url+'.json';
    return $.ajax({
     type: type,
     async: false,
     url: url,
     data: opt,
-    dataType: 'json',
-    success: function(data) {
-     console.log(data);
-     return data;
-    }
+    dataType: 'json'
    }).responseText;
   },
 
-  getEntry : function(id,opt) {
+  //Entries
+  getEntry : function(id, opt) {
    return this.apiClient('getEntry',[id],opt);
   },
+  prevEntry : function(id, opt) {
+   return this.apiClient('prevEntry',[id],opt);
+  },
+  nextEntry : function(id, opt) {
+   return this.apiClient('nextEntry',[id],opt);
+  },
+  lastEntry : function(opt) {
+   return this.apiClient('lastEntry',NULL,opt);
+  },
 
-  commentCount : function(id) {
-   return this.apiClient('commentCount',[id]);
-  }
+  //Comments
+  commentCount : function(postid, opt) {
+   return this.apiClient('commentCount',[postid],opt);
+  },
+  getComments : function(postid, opt) {
+   return this.apiClient('getComments',[postid],opt);
+  },
+  postComment : function(postid, opt) {
+   return this.apiClient('postComment',[postid],opt);
+  },
+
+  //Categories
+  getCatID : function(entryid, opt) {
+   return this.apiClient('getCatID',[entryid],opt);
+  },
+  getCat : function(catid, opt) {
+   return this.apiClient('getCat',[catid],opt);
+  },
+
+  //Authentication
+  getUser : function(value, opt) {
+   return this.apiClient('getUser',[value],opt);
+  },
+  login : function(opt) {
+   return this.apiClient('getCat',NULL,opt);
+  },
+  logout : function() {
+   return this.apiClient('logout');
+  },
+  getAuthUser : function() {
+   return this.apiClient('getAuthUser');
+  },
+
  };
 })(jQuery);
