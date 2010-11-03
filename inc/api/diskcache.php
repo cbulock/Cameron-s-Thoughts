33b9acb215aa2ee8cc28e16485f3339eeceac904
Cameron Bulock <cameron@bulock.com>
2010-11-03 13:49:48 -0400

    added '.cache' to the end of cache files

--- inc/api/diskcache.php
+++ inc/api/diskcache.php
@@ -8,17 +8,17 @@ public function exists($name, $expires = DEFAULT_CACHE_EXPIRES) {//NULL expires 
  $name = md5($name);
  if ($expires) {
   $mtime = @filemtime(CACHE_DIR.$name);
-  if ((date('U') - $mtime) < $expires*60 && file_exists(CACHE_DIR.$name)) return TRUE;
+  if ((date('U') - $mtime) < $expires*60 && file_exists(CACHE_DIR.$name.'.cache')) return TRUE;
  }
  else {
-  if (file_exists(CACHE_DIR.$name)) return TRUE;
+  if (file_exists(CACHE_DIR.$name.'.cache')) return TRUE;
  }
  return FALSE;
 }
 
 public function add($name, $data) {
  $name = md5($name);
- $file = fopen(CACHE_DIR.$name,'w'); 
+ $file = fopen(CACHE_DIR.$name.'.cache','w'); 
  fwrite($file,serialize($data));
  fclose($file);
  return TRUE;
@@ -27,7 +27,7 @@ public function add($name, $data) {
 public function read($name) {
  if (!$this->exists($name,NULL)) return FALSE;
  $name = md5($name);
- $filename = CACHE_DIR.$name;
+ $filename = CACHE_DIR.$name.'.cache';
  $file = fopen($filename,'r');
  $this->count++;
  return unserialize(fread($file,filesize($filename)));
