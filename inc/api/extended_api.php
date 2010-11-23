dd9c2659357e3c1863716e5cda5f6167107b00a7
Cameron Bulock <cameron@bulock.com>
2010-11-23 13:08:19 -0500

    made some more methods blogid aware

--- inc/api/extended_api.php
+++ inc/api/extended_api.php
@@ -6,15 +6,16 @@ public function getNewEntries($options = array()) {
  $setup['options'] = $options;
  $setup['defaults'] = array(
   //'offset' => '0', //don't think offset will be implemented for this method
-  'count' => '10'
+  'count' => '10',
+  'blogid' => '2'
  );
  extract($setup_result = $this->api_call_setup($setup));
  $entries = array();
- $current_entry = $this->lastEntry();
+ $current_entry = $this->lastEntry(array('blogid'=>$options['blogid']));
  for($i = 0; $i < $options['count']; $i++) {
   $entries[$current_entry] = $this->getEntry($current_entry,array('callby'=>'id'));
   if ($i < $options['count']-1) {
-   $current_entry = $this->prevEntry($current_entry);
+   $current_entry = $this->prevEntry($current_entry,array('blogid'=>$options['blogid']));
   }
  }
  return $this->api_call_finish($entries);
@@ -24,11 +25,12 @@ public function getCatEntries($catid, $options = array()) {
  $setup['options'] = $options;
  $setup['defaults'] = array(
   'offset' => '0',//offset and count are not implemented in the sql query yet
-  'count' => '10'
+  'count' => '10',
+  'blogid' => '2'
  );
  extract($setup_result = $this->api_call_setup($setup));
  $dboptions = array(
-  'where' => 'placement_category_id = '.$this->db->sqlClean($catid),
+  'where' => 'placement_category_id = "'.$this->db->sqlClean($catid).'" AND placement_blog_id= "'.$this->db->sqlClean($options['blogid']).'"',
   'orderBy' => 'placement_entry_id DESC',
   'key' => 'placement_entry_id'
  );
@@ -43,11 +45,12 @@ public function getMonthlyEntries($month, $year, $options = array()) {
  $setup['options'] = $options;
  $setup['defaults'] = array(
   'offset' => '0',//offset and count are not implemented in the sql query yet
-  'count' => '10'
+  'count' => '10',
+  'blogid' => '2'
  );
  extract($setup_result = $this->api_call_setup($setup));
  $dboptions = array(
-  'where' => "MONTH(entry_created_on) = ".$month." AND YEAR(entry_created_on) = ".$year,
+  'where' => 'MONTH(entry_created_on) = "'.$month.'" AND YEAR(entry_created_on) = "'.$year.'" AND entry_blog_id= "'.$this->db->sqlClean($options['blogid']).'"',
   'orderBy' => 'entry_id DESC',
   'key' => 'entry_id'
  );
