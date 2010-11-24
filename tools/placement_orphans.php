ebb565dfea8173b1b426f2b9b5e8ffde3e33e34d
Cameron Bulock <cameron@bulock.com>
2010-11-24 15:53:00 -0500

    created tool to find orphan placement entries in mt_placement

---
+++ tools/placement_orphans.php
@@ -0,0 +1,21 @@
+<?php
+////////////////////////////////////////////////////////////////////
+// This finds any entries in the mt_placement table that don't have
+// corresponding entries in mt_entries and prints the entry id's 
+////////////////////////////////////////////////////////////////////
+
+echo "The following entry ID's are found in mt_placement but are not in mt_entry:\n\n";
+
+require_once('../var.inc');
+require_once('../inc/api/mysql.php');
+require_once('../inc/api/cache.php');
+$cache = new Cache;
+$db = new DB($settings['db']['host'],$settings['db']['user'],$settings['db']['pass'],array('prefix'=>DB_PREFIX,'cache'=>$cache));
+
+$placements = $db->getTable('mt_placement',array('orderBy'=>"`placement_id`",'key'=>'placement_id','cache'=>FALSE));
+
+foreach($placements as $p) {
+ if(!$db->getItem('mt_entry',$p['placement_entry_id'],array('field'=>'entry_id','cache'=>FALSE))) {
+  echo $p['placement_entry_id']."\n";
+ }
+} 
