2ffa7cdc4485edaeba7fef9cda01e862ed6a60f7
Cameron Bulock <cameron@bulock.com>
2011-03-04 18:00:55 -0500

    missing $

--- js/main.js
+++ js/main.js
@@ -124,7 +124,7 @@ function postCommentListener() {
    $('#comment_submit').fadeOut();
    $('#leave_comment').slideUp();
    snippetLoad('comment_footer',function() {
-    ('#comment_body').append(arguments[0]);
+    $('#comment_body').append(arguments[0]);
    },comment.id);
    $('#comments').html(call('commentCountText',comment.count));
   }
