8bc10689c8ab63ac2fee94a2b20952520ccb4088
Cameron Bulock <cameron@bulock.com>
2011-06-08 16:48:18 -0400

    removed methods that did direct api calls

--- js/jquery.ct.js
+++ js/jquery.ct.js
@@ -1,5 +1,6 @@
 (function($){
  $.ct = {
+
   apiClient : function (method, req, opt) {
    type = 'GET';
    if (opt) {
@@ -29,76 +30,6 @@
    req = req || null;
    opt = opt || null;
    return this.apiClient(method, req, opt);
-  },
-
-  //Entries
-  getEntry : function(id, opt) {
-   return this.apiClient('getEntry',[id],opt);
-  },
-  postEntry : function(opt) {
-   return this.apiClient('postEntry',null,opt);
-  },
-  prevEntry : function(id, opt) {
-   return this.apiClient('prevEntry',[id],opt);
-  },
-  nextEntry : function(id, opt) {
-   return this.apiClient('nextEntry',[id],opt);
-  },
-  lastEntry : function(opt) {
-   return this.apiClient('lastEntry',null,opt);
-  },
-  getNewEntries : function(opt) {
-   return this.apiClient('getNewEntries',null,opt);
-  },
-  getMonthlyEntries : function(month,year,opt) {
-   return this.apiClient('getMonthlyEntries',[month],[year],opt);//don't think this will work, the reqs should be one array
-  },
-
-  //Comments
-  commentCount : function(postid, opt) {
-   return this.apiClient('commentCount',[postid],opt);
-  },
-  commentCountText : function(count, opt) {
-   return this.apiClient('commentCountText',[count],opt);
-  },
-  getComments : function(postid, opt) {
-   return this.apiClient('getComments',[postid],opt);
-  },
-  postComment : function(postid, opt) {
-   return this.apiClient('postComment',[postid],opt);
-  },
-
-  //Categories
-  getCatID : function(entryid, opt) {
-   return this.apiClient('getCatID',[entryid],opt);
-  },
-  getCat : function(catid, opt) {
-   return this.apiClient('getCat',[catid],opt);
-  },
-  getCatEntries : function(catid, opt) {
-   return this.apiClient('getCatEntries',[catid],opt);
-  },
-  getCatList : function() {
-   return this.apiClient('getCatList',null,opt);
-  },
-
-  //Authentication
-  getUser : function(value, opt) {
-   return this.apiClient('getUser',[value],opt);
-  },
-  login : function(username, opt) {
-   return this.apiClient('login',[username],opt);
-  },
-  logout : function() {
-   return this.apiClient('logout');
-  },
-  getAuthUser : function() {
-   return this.apiClient('getAuthUser');
-  },
-
-  //Status
-  getLatestStatus : function() {
-   return this.apiClient('getLatestStatus');
   }
 
  };
